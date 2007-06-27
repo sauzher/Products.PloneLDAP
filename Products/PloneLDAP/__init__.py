98d99644d5a3ccc2f930c4bcf73a6a576c845d77
Wichert Akkerman <wichert@wiggy.net>
2007-06-27 17:02:02 +0000

    More bits of AD support
    svn path=/PloneLDAP/trunk/; revision=44703

--- Products/PloneLDAP/__init__.py
+++ Products/PloneLDAP/__init__.py
@@ -2,10 +2,12 @@ from AccessControl.Permissions import add_user_folders
 from Products.PluggableAuthService.PluggableAuthService import \
                 registerMultiPlugin
 from Products.PloneLDAP.plugins.ldap import PloneLDAPMultiPlugin
+from Products.PloneLDAP.plugins.ad import PloneActiveMultiPlugin
 from Products.PloneLDAP.factory import manage_addPloneLDAPMultiPluginForm,\
                 manage_addPloneLDAPMultiPlugin
 
 registerMultiPlugin(PloneLDAPMultiPlugin.meta_type)
+registerMultiPlugin(PloneActiveDirectoryMultiPlugin.meta_type)
 
 def initialize(context):
 
