ebf1d247287f763df4baa7132026a2c5fc4e324c
Maurits van Rees <maurits@vanrees.org>
2012-11-30 11:05:26 +0100

    cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 from setuptools import setup, find_packages
-import os.path
 
-version = '1.2'
+version = '1.2.dev.0'
 
 setup(name='Products.PloneLDAP',
       version=version,
       description="LDAP/Active Directory support for Plone",
-      long_description=open('README.txt').read() +
-                      open(os.path.join('docs', 'CHANGES.txt')).read(),
+      long_description=(open('README.txt').read() +
+                        open('CHANGES.rst').read()),
       classifiers=[
-        "Framework :: Plone",
-        "Framework :: Zope2",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: System :: Systems Administration :: Authentication/Directory :: LDAP",
-        "License :: OSI Approved :: Zope Public License",
-      ],
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: System :: Systems Administration :: Authentication/Directory :: LDAP",
+          "License :: OSI Approved :: Zope Public License",
+          ],
       keywords='Zope CMF Plone LDAP authentication',
       author='Wichert Akkerman - Simpon',
       author_email='wichert@simplon.biz',
@@ -26,10 +25,9 @@ setup(name='Products.PloneLDAP',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-        'setuptools',
-        'Products.PlonePAS',
-        'Products.LDAPMultiPlugins',
-        'Products.LDAPUserFolder',
-      ],
-)
-
+          'setuptools',
+          'Products.PlonePAS',
+          'Products.LDAPMultiPlugins',
+          'Products.LDAPUserFolder',
+          ],
+      )
