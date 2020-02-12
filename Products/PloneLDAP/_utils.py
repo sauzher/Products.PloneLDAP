834cf621da419093f34e1d2bc00be869673cb621
ale-rt <alessandro.pisa@gmail.com>
2020-02-12 15:34:13 +0100

    Use safe_unicode which is used in Plone since years

--- Products/PloneLDAP/_utils.py
+++ Products/PloneLDAP/_utils.py
@@ -1,36 +1,9 @@
-# In case CMFPlone.utils is available we use it, otherwise we define our own,
-# which is a plain copy of it, as of 2010-07-21
-try:
-    from Products.CMFPlone.utils import safe_unicode
-    safe_unicode  # pyflakes
-except ImportError:
-    def safe_unicode(value, encoding='utf-8'):
-        """Converts a value to unicode, even it is already a unicode string.
+# coding=utf-8
+import zope.deferredimport
 
-            >>> from Products.CMFPlone.utils import safe_unicode
+zope.deferredimport.initialize()
 
-            >>> safe_unicode('spam')
-            u'spam'
-            >>> safe_unicode(u'spam')
-            u'spam'
-            >>> safe_unicode(u'spam'.encode('utf-8'))
-            u'spam'
-            >>> safe_unicode('\xc6\xb5')
-            u'\u01b5'
-            >>> safe_unicode(u'\xc6\xb5'.encode('iso-8859-1'))
-            u'\u01b5'
-            >>> safe_unicode('\xc6\xb5', encoding='ascii')
-            u'\u01b5'
-            >>> safe_unicode(1)
-            1
-            >>> print safe_unicode(None)
-            None
-        """
-        if isinstance(value, unicode):
-            return value
-        elif isinstance(value, basestring):
-            try:
-                value = unicode(value, encoding)
-            except (UnicodeDecodeError):
-                value = value.decode('utf-8', 'replace')
-        return value
+zope.deferredimport.deprecated(
+    "Import from Products.CMFPlone.utils instead",
+    safe_unicode='Products.CMFPlone.utils:safe_unicode',
+)
