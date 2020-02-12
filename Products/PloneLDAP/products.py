0f29671eab88f493edd58a8439f33921b62b2d27
ale-rt <alessandro.pisa@gmail.com>
2020-02-12 15:01:06 +0100

    Fix implementer decorator

--- Products/PloneLDAP/products.py
+++ Products/PloneLDAP/products.py
@@ -1,8 +1,8 @@
 from zope.interface import implementer
 from Products.CMFQuickInstallerTool.interfaces import INonInstallable
 
+@implementer(INonInstallable)
 class HiddenLDAPProducts(object):
-    implementer(INonInstallable)
 
     def getNonInstallableProducts(self):
         return [ "LDAPUserFolder" ]
